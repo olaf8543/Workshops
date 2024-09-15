f33ce069a8648a2e0f3a8ec0632817c0b11256d7
saltyypanda <esh7943@g.rit.edu>
2024-09-15 10:31:47 -0400

    Remove solution.ts

--- workshop-02-typescript/practice-problems/problem-2/solution.ts
+++
@@ -1,6 +0,0 @@
-function displayUserInfo(user: { name: string, age: number, favoriteFoods: string[] }): void {
-    console.log(`Name: ${user.name}`);
-    console.log(`Age: ${user.age}`);
-    console.log("Favorite Foods:");
-    user.favoriteFoods.forEach((food) => console.log(food));
-  }
