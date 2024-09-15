ce40d58b58b4643eeb18f18397aa2c52c05cc25b
Owen Lafferty <magnaman8585@gmail.com>
2024-09-15 12:19:57 -0400

    finished problem 1

---
+++ workshop-02-typescript/practice-problems/problem-1/main.js
@@ -0,0 +1,11 @@
+// Fill in for exercises!
+var userName = 'Owen';
+var favoriteFoods = ["Ice Cream", "burritos", "Lo mein"];
+var user = {
+    name: userName,
+    age: 18,
+    favoriteFoods: favoriteFoods
+};
+
+console.log(user)
+console.log("One Food: " + favoriteFoods[0])
--- workshop-02-typescript/practice-problems/problem-1/main.ts
+++ workshop-02-typescript/practice-problems/problem-1/main.ts
@@ -1,1 +1,12 @@
 // Fill in for exercises!
+let userName: string = 'Owen'
+let favoriteFoods: string[] = ["Ice Cream", "burritos", "Lo mein"]
+
+let user: {name: string, age: number, favoriteFoods: string[]} = {
+    name: userName,
+    age: 18,
+    favoriteFoods: favoriteFoods
+}
+
+console.log(user)
+console.log(favoriteFoods[0])
